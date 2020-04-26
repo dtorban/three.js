2da600690593c2b9086950cade4f4e0572236639
Bridger Herman <herma582@umn.edu>
2020-04-26 17:23:54 -0500

    Add native webgl texture code for ssao

--- examples/jsm/postprocessing/SSAOPass.js
+++ examples/jsm/postprocessing/SSAOPass.js
@@ -25,6 +25,7 @@ import {
 	Vector3,
 	WebGLRenderTarget,
 	ZeroFactor,
+	Texture,
 } from "../../../build/three.module.js";
 import { Pass } from "../postprocessing/Pass.js";
 import { SimplexNoise } from "../math/SimplexNoise.js";
@@ -33,7 +34,59 @@ import { SSAOBlurShader } from "../shaders/SSAOShader.js";
 import { SSAODepthShader } from "../shaders/SSAOShader.js";
 import { CopyShader } from "../shaders/CopyShader.js";
 
-var SSAOPass = function ( scene, camera, width, height ) {
+// Source:
+// https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Using_textures_in_WebGL
+function loadTexture(gl) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Because images have to be download over the internet
+  // they might take a moment until they are ready.
+  // Until then put a single pixel in the texture so we can
+  // use it immediately. When the image has finished downloading
+  // we'll update the texture with the contents of the image.
+  const level = 0;
+  const internalFormat = gl.RGBA;
+  const width = 1;
+  const height = 1;
+  const border = 0;
+  const srcFormat = gl.RGBA;
+  const srcType = gl.UNSIGNED_BYTE;
+  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                width, height, border, srcFormat, srcType,
+                pixel);
+
+  const image = new Image();
+  image.onload = function() {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                  srcFormat, srcType, image);
+
+    // WebGL1 has different requirements for power of 2 images
+    // vs non power of 2 images so check if the image is a
+    // power of 2 in both dimensions.
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+       // Yes, it's a power of 2. Generate mips.
+       gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+       // No, it's not a power of 2. Turn off mips and set
+       // wrapping to clamp to edge
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+  };
+//   image.src = url;
+
+  return texture;
+}
+
+function isPowerOf2(value) {
+  return (value & (value - 1)) == 0;
+}
+
+var SSAOPass = function ( scene, camera, width, height, renderer ) {
 
 	Pass.call( this );
 
@@ -110,10 +163,17 @@ var SSAOPass = function ( scene, camera, width, height ) {
 		blending: NoBlending
 	} );
 
+	var texture = new Texture();
+	var properties = renderer.properties.get( texture );
+	properties.__webglTexture = loadTexture(renderer.context);
+	properties.__webglInit = true;
+
+	// this.normalTex = texture;
 	this.normalTex = new TextureLoader().load('textures/webgl_postprocessing_ssao_normal.png');
 	this.normalTex.minFilter = NearestFilter;
 	this.normalTex.magFilter = NearestFilter;
-	this.diffuseTex = new TextureLoader().load('textures/webgl_postprocessing_ssao_diffuse.png');
+	// this.diffuseTex = new TextureLoader().load('textures/webgl_postprocessing_ssao_diffuse.png');
+	this.diffuseTex = texture;
 	this.depthTex = new TextureLoader().load('textures/webgl_postprocessing_ssao_depth2.png', (tex) => {
 		console.log(tex);
 	});
